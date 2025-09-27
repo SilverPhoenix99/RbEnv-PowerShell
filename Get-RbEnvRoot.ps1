22db916e8c9e5dd54e153c44a478dfdb116003a4
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2025-09-27 22:36:07 +0100

    Fix indentation.

--- Get-RbEnvRoot.ps1
+++ Get-RbEnvRoot.ps1
@@ -13,7 +13,7 @@ function Get-RbEnvRoot {
     $ErrorActionPreference = [Management.Automation.ActionPreference]::Stop
 
     try {
-    if ($Env:RBENV_ROOT) {
+        if ($Env:RBENV_ROOT) {
 
             $root = Get-Item $Env:RBENV_ROOT -Force -ErrorAction Ignore
             if ($root.PSIsContainer) {
@@ -22,7 +22,7 @@ function Get-RbEnvRoot {
 
             Write-Error "not a directory (`$Env:RBENV_ROOT): $Env:RBENV_ROOT" `
                 -RecommendedAction 'Set $Env:RBENV_ROOT to a valid directory.'
-    }
+        }
 
         $rootPath = Join-Path $HOME .rbenv
         $root = Get-Item $rootPath -Force -ErrorAction Ignore
