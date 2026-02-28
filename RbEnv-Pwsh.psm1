c16b6594b20dcc5aaccad695f10a3a6ea8ba1915
SilverPhoenix99 <antoniopedrosilvapinto@gmail.com>
2026-02-28 23:21:45 +0000

    Ensure model dependencies are sourced in the correct order.

--- RbEnv-Pwsh.psm1
+++ RbEnv-Pwsh.psm1
@@ -1,13 +1,26 @@
 
-$scriptName = (Get-Item $PSCommandPath).BaseName
+$ErrorActionPreference = [Management.Automation.ActionPreference]::Stop
 
-$psd1File = Join-Path $PSScriptRoot "$scriptName.psd1"
-$psd1 = Import-PowerShellDataFile -Path $psd1File
+(& {
+    # Sorted by dependencies:
+    Join-Path $PSScriptRoot 'model' 'RbEnvLocationChanged.ps1'
+    Join-Path $PSScriptRoot 'model' 'RubyConfiguration.ps1'
+    Join-Path $PSScriptRoot 'model' 'RubyVersion.ps1'
+    Join-Path $PSScriptRoot 'model' 'RubyVersionDescriptor.ps1'
 
-Get-ChildItem $PSScriptRoot -Recurse -Include *.ps1 `
-    | ForEach-Object { . $_.FullName }
+    Get-ChildItem (Join-Path $PSScriptRoot 'private') -File -Recurse -Include *.ps1 | ForEach-Object FullName
+    Get-ChildItem (Join-Path $PSScriptRoot '*.ps1') -File | ForEach-Object FullName
 
-Export-ModuleMember `
-    -Variable $psd1.VariablesToExport `
-    -Function $psd1.FunctionsToExport `
-    -Alias    $psd1.AliasesToExport
+}).ForEach{ . $_ }
+
+& {
+    $scriptName = Split-Path $PSCommandPath -LeafBase
+
+    $psd1File = Join-Path $PSScriptRoot "$scriptName.psd1"
+    $psd1 = Import-PowerShellDataFile -Path $psd1File
+
+    Export-ModuleMember `
+        -Variable $psd1.VariablesToExport `
+        -Function $psd1.FunctionsToExport `
+        -Alias    $psd1.AliasesToExport
+}
